8bae949c871c5c7718cc17fd37cc7a9cdd3fe3dd
Daniil <severustime@gmail.com>
2019-10-10 23:53:10 +0500

    create models

--- picxel/mainapp/models.py
+++ picxel/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Tags(models.Model):
+    name = models.CharField(max_length=70)
+
+
+class Image(models.Model):
+    tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    source = models.CharField(max_length=70)
+    author = models.CharField(max_length=70)
+    license = models.CharField(max_length=70)
+    pic = models.ImageField(upload_to='pic/')
